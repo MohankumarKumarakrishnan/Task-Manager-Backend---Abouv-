9639bfc0ec08d2d517ac352b7e90442d12320f72
sarav <mkmohankumar261998@gmail.com>
2025-03-27 19:57:37 +0530

    Backend created successfully and served the front end react from backend

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const path = require("path");
 const PORT = 5500
+const cors = require('cors');
+
+app.use(
+    cors({
+      origin: "*",
+      credentials: true,
+    })
+  );
 
 
-app.use('/',(req,res)=>{
-    res.send("<h1>Hello world</h1>")
-})
+
+
+
+if(process.env.ENVIRONMENT === 'Production'){
+    app.use(express.static(path.join(__dirname, "../Task Manager/dist")))
+    app.use("/", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../Task Manager", "dist", "index.html"));
+      });
+}
 
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
